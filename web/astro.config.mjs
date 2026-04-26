360998990c976272ffe26a81fe702d9505ab4962
Kuro <hello@kurojifusky.com>
2026-04-26 20:30:02 +0800

    add cloudflare adapter

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -4,15 +4,20 @@ import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 import svelte from '@astrojs/svelte';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+
   vite: {
     plugins: [tailwindcss()]
   },
+
   prefetch: {
     prefetchAll: true,
   },
 
-  integrations: [sitemap(), svelte()]
+  integrations: [sitemap(), svelte()],
+  adapter: cloudflare()
 });
